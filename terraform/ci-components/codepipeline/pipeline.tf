43be84e8197ca414dec0d58435e075f8c394be3b
Mark Butler <mark.butler@solirius.com>
2021-03-17 13:57:48 +0000

    add the action_provider and action_category to the for_each loop variables

--- terraform/ci-components/codepipeline/pipeline.tf
+++ terraform/ci-components/codepipeline/pipeline.tf
@@ -49,6 +49,8 @@ resource "aws_codepipeline" "pipeline" {
               namespace = myAction.namespace
               codebuild_name = myAction.codebuild_name
               action_env = myAction.action_env
+              action_provider = myAction.action_provider
+              action_category = myAction.action_category
             }
           ]
         ])
@@ -56,11 +58,9 @@ resource "aws_codepipeline" "pipeline" {
         content {
           #name = action.value.action_name + var.name_suffix
           name = action.value.action_name
-          #category         = length(action.value.action_category) > 1 ? action.value.action_category : "Build"
-          category = "Build"
+          category = action.value.action_category
           owner = "AWS"
-          #provider         = length(action.value.action_provider) > 1 ? action.value.action_provider : "AWS"
-          provider = "AWS"
+          provider = action.value.action_provider
           version = "1"
           run_order = 3
           input_artifacts = [action.value.input_artifacts]
